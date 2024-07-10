9defed10175a587df72dc382369e0fa68f632815
kuba <hladikjakubis@gmail.com>
2024-07-10 17:00:31 +0200

    first stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,64 @@
+"use client"
+import useMouse from "@react-hook/mouse-position";
+import { MotionValue, motion, useMotionTemplate, useMotionValue, useSpring, useTransform } from "framer-motion";
 import Image from "next/image";
-
+import { ButtonHTMLAttributes, forwardRef, useEffect, useRef, useState } from "react";
+import "@/app/home.css"
+import { cn } from "@/lib/utils";
 export default function Home() {
+  const Button = forwardRef<HTMLButtonElement, ButtonHTMLAttributes<HTMLButtonElement>>((props, ref) => {
+    const buttonref = useRef<HTMLDivElement>(null);
+    const { x, y, elementWidth, elementHeight } = useMouse(buttonref, {});
+    const valueX = useMotionValue(0);
+    const moveFactor = 0.4;
+    const transformX = useTransform(valueX, [-1, 1], [-elementWidth! * moveFactor, elementWidth! * moveFactor]);
+    const springX = useSpring(transformX, {stiffness: 300, damping: 50});
+    const valueY = useMotionValue(0);
+    const transformY = useTransform(valueY, [-1, 1], [-elementHeight! * moveFactor, elementHeight! * moveFactor]);
+    const springY = useSpring(transformY, {stiffness: 300, damping: 50});
+    const [hover, setHover] = useState(false);
+    useEffect(() => {
+      if (!hover){
+        valueX.set(0)
+        valueY.set(0)
+        return
+      }
+      valueX.set(((x! / elementWidth!) *2) -1  || 0 )
+    }, [x, hover]);
+    useEffect(() => {
+      valueY.set(((y! / elementHeight!) *2) -1 || 0 )
+    }, [y, hover]);
+return (
+  <motion.div 
+    ref={buttonref} 
+    initial={{borderRadius: "0.8rem", scale: 1}}
+    className={cn("relative w-fit h-fit bg-blue-400 p-2", props.className)} 
+    whileHover={{borderRadius: "1.3rem", scale:1.2, zIndex: 40}} 
+    transition={{
+      type: "spring",
+      stiffness: 260,
+      damping: 50
+    }} 
+    style={{x:springX, y: springY}}
+    onMouseEnter={() => setHover(true)}
+    onMouseLeave={() => setHover(false)}
+  >
+    <button ref={ref} {...props} className=" hover:z-50 bg-transparent w-full h-full select-none">
+      {props.children}
+    </button>
+    <div className="absolute w-full h-full pointer-events-none"> 
+    </div>
+  </motion.div>
+)
+  })
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="grid min-h-screen place-content-center w-screen bg-black  p-24">
+      {/* <div className=" h-96 w-64 border-4 rounded-3xl p-4"> */}
+        {/* <Image className=" rounded-xl" alt="test" src="https://cdn.myshoptet.com/usr/www.planetayurveda.cz/user/shop/big/74-2_planetayurveda-arjuna.jpg?65ec8c9e"  width={500} height={500} /> */}
+      <Button className=" bg-orange-500" onClick={()=> console.log("fuck")}>
+        hello
+      </Button>
+    {/* </div> */}
     </main>
   );
 }
